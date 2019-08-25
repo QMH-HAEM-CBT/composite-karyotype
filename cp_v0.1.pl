f7e46718a7d718bb8d7069d62b6ce5937ed98fe6
QMH-HAEM <ihw426@ha.org.hk>
2019-08-25 12:34:47 +0800

    Added draft of cp_v0.1.pl that can preliminary read CSV file from MetaSystem

---
+++ cp_v0.1.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+################################################################################
+# This script takes the CSV output from MetaSystem and convert the karyotypes
+# from individual metaphases to a composite karyotype in the format of ISCN.
+#
+# Author: Clinical Bioinformatics Team, Cytogenetics and Genomics Laboratory,
+#         Division of Haematology, Queen Mary Hospital
+#
+# Creation Date: 25/08/2019
+# Modified Date: 25/08/2019
+#
+# v0.1: Prototypical version
+################################################################################
+
+use strict;
+use warnings;
+
+$/ = "\r\n";   # Change the default new line character from LF to CRLF to take input from MetaSystem
+
+open IN, $ARGV[0], or die $!;
+<IN>;
+while(<IN>){
+   chomp;
+   my @fields = split '";"';  # Split by ";", instead of just by semicolon
+
+   $fields[0] =~ s/^"//;   # Remove the leading quotation mark
+   $fields[$#fields] =~ s/"$//;  # Remove the trailing quotation mark
+
+   print (join "\t", @fields);
+   print "\n";
+}
+close IN;
